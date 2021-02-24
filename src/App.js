72dbca1512e8dc4853d61295fb75476595d3ae98
Farukhrana14 <farukhrana14@gmail.com>
2021-02-24 08:39:08 +0600

    added random user from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Users>    </Users>
       </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
+
+
+function Users() {
+  const personStyle = {
+    border: '5px solid white',
+    borderRadius: '10px',
+    margin: '10px',
+    padding: '10px',
+    width: '550px',
+    display: 'inline-grid',
+  }
+ 
+  const imgStyle = {
+    borderRadius: '10px',
+  }
+
+  const divImgStyle = {
+    textAlign: 'right',
+  }
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // fetch('https://randomuser.me/api/?results=50')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  console.log(users);
+
+  return (
+    <div>
+      <div><h3>Random Users: {users.length}</h3></div>
+      <div>
+        {users.map(user => <div style={personStyle}> <div style={{textAlign:'left'}} >Name: {user.name} <br/> Email: {user.email} <br/>Phone: {user.phone} <br/>Website: {user.website} <br/> Company: {user.company.name} <br/> City: {user.address.city} </div>  <div style={divImgStyle}> <img style={imgStyle} src={"https://via.placeholder.com/150"} /> </div>  </div>)}
+      
+        
+      </div>
+
+
+    </div>
+
+
+  )
+
+}
+
+
 
 export default App;
